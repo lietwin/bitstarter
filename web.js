d2ddec40127b7b3bcd78ee3edf9af4db8db60a42
lietwin <quicklics@gmail.com>
2013-07-07 21:59:05 +0000

    Updated web.js to handle index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  fs.readFile('index.html', function(err, data){
+  if(err) throw err;
+    response.send(new Buffer(data).toString('utf8'));
+    //console.log(new Buffer(data).toString('utf8'));
+});
+
 });
 
 var port = process.env.PORT || 5000;
