071b10dd84438d2b942c27b65ffd7a15c546fcc7
lietwin <quicklics@gmail.com>
2013-07-07 22:13:14 +0000

    Updated index.html with bitstarter html grid layout

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  fs.readFile('index.html', function(err, data){
+  fs.readFileSync('index.html', function(err, data){
   if(err) throw err;
     response.send(new Buffer(data).toString('utf8'));
     //console.log(new Buffer(data).toString('utf8'));
